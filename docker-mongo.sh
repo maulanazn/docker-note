e94988c4fbbb04d919d116d7531e207b0d19cea0
maulanazn <starknntnk99@gmail.com>
2023-10-24 06:50:08 +0700

    Fixing the docker restore and backup, the problem is that I do not stop the mongodb server that I run from docker container.

--- docker-mongo.sh
+++ docker-mongo.sh
@@ -1,9 +1,15 @@
 docker image pull mongo:jammy
 
-docker container create --name mongo-service --publish 8080:27017 --mount "type=volume,source=mongo-volume,destination=/data"  --memory 450m --cpus 1.0  mongo:jammy
+docker volume create surakarta-volume
 
-docker container run --rm --name ubuntumongo-backup  --mount "type=bind,source=/home/maulanazn/Devstack/nyobaindocker/server-postgres-1/backup,destination=/backup" --mount "type=volume,source=mongo-volume,destination=/data" ubuntu:latest tar cvf /backup/mongo-server-1.tar.gz /data
+docker container run -d --name mongo-server-surakarta --publish 8080:27017 --volume surakarta-volume:/data/db --env MONGO_INITDB_ROOT_USERNAME=maulanazn --env MONGO_INITDB_ROOT_PASSWORD=maulanazn123 mongo:jammy
 
-docker container run --rm --name ubuntumongo-restore --mount "type=bind,source=/home/maulanazn/Devstack/nyobaindocker/server-postgres-1/backup,destination=/backup" --mount "type=volume,source=mongo-new-volume,destination=/data/db" ubuntu:latest bash -c "cd /data && tar xvf /backup/mongo-server-1.tar.gz --strip 1"
+docker container run --rm --name ubuntumongo-backup  --mount "type=bind,source=/home/maulanazn/Devstack/nyobaindocker/server-postgres-1/backup,destination=/backup" --volume surakarta-volume:/data ubuntu:latest tar cvf /backup/mongo-server-1.tar.gz /data/db
 
-docker container create --name mongo-new-service --publish 8080:27017 --mount "type=volume,source=mongo-new-volume,destination=/data" --env MONGO_INITDB_ROOT_USERNAME=maulanazn --env MONGO_INITDB_ROOT_PASSWORD=maulanazn123 --memory 450m --cpus 1.0  mongo:jammy
+docker volume create bogor-volume
+
+docker container run --rm --name ubuntumongo-restore --mount "type=bind,source=/home/maulanazn/Devstack/nyobaindocker/server-postgres-1/backup,destination=/backup" --volume bogor-volume:/data ubuntu:latest bash -c "cd /data && tar xvf /backup/mongo-server-1.tar.gz --strip 1"
+
+docker container run -d --name mongo-server-bogor --publish 8080:27017 --volume bogor-volume:/data/db --env MONGO_INITDB_ROOT_USERNAME=maulanazn --env MONGO_INITDB_ROOT_PASSWORD=maulanazn123 mongo:jammy
+
+docker container stop mongo-server-bogor
