756ab1f904f369d900776f995acca314dea2dd56
maulanazn <starknntnk99@gmail.com>
2023-10-24 18:22:22 +0700

    docker health check

--- check-health/server.go
+++ check-health/server.go
@@ -6,12 +6,26 @@ import (
 	"os"
 )
 
+var counter int
+
 func HelloServer(writer http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(writer, "Hello world")
+}
+
+func Counter(writer http.ResponseWriter, req *http.Request) {
+	counter++
+
+	if counter > 5 {
+		writer.WriteHeader(500)
+		fmt.Fprint(writer, "KO")
+	} else {
+		fmt.Fprint(writer, "Hello world")
+	}
 }
 
 func main() {
 	port := os.Getenv("HOST_PORT")
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/trigger", Counter)
 	http.ListenAndServe(":"+port, nil)
 }
